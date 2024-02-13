05050a4fb1362f88c3f3ec56733b3410b74a5528
Donikian Chahan <chahan.donikian@cpe.fr>
2024-02-13 19:24:33 +0100

    ajout des commentaires sur la fonction RPS_MultipleGame

--- Game/RPS_Tools/RPS_MultipleGame.py
+++ Game/RPS_Tools/RPS_MultipleGame.py
@@ -2,12 +2,24 @@ from RPS_SimpleGame import RPS_SimpleGame
 import random
 
 class RPS_MultipleGame:
+    """
+    Classe permettant de jouer plusieurs parties de Pierre/Feuille/Ciseaux contre l'ordinateur.
+    Elle garde un historique des parties jouées, y compris les choix et les résultats.
+    """
     def __init__(self, player_name):
+        """
+        Initialise l'instance de RPS_MultipleGame.
+
+        :param player_name: Nom du joueur humain.
+        """
         self.game = RPS_SimpleGame()
         self.history = []
         self.player_name = player_name 
     
     def play_game(self):
+        """
+        Permet au joueur de jouer une partie contre l'ordinateur et enregistre le résultat.
+        """
         player_choice = input(f"{self.player_name}, Veuillez choisir [R]ock, [P]aper, [S]cissors: ").upper()
         if player_choice not in ['R', 'P', 'S']:
             print("Choix invalide")
@@ -33,5 +45,10 @@ class RPS_MultipleGame:
             print(f"L'ordinateur gagne ! il avait choisi {computer_choice}.")
 
     def get_history(self):
+        """
+        Retourne l'historique des parties jouées.
+
+        :return: Liste des enregistrements de chaque partie jouée.
+        """
         return self.history
     
